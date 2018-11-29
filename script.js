e359e2a81da9f15c449923be9ede134c72e8c960
Ricky Severino <rickyseverino@protonmail.com>
2018-11-29 18:23:37 -0500

    add login and register page

--- script.js
+++ script.js
@@ -3,5 +3,31 @@ $(document).ready(function() {
     $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
     $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
 
-      
+
+	    // Get the modal
+	var modal = document.getElementById('loginModal');
+
+	// Get the button that opens the modal
+	var loginLink = document.getElementById("login");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks the button, open the modal 
+	loginLink.onclick = function() {
+	    modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	    modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	    if (event.target == modal) {
+	        modal.style.display = "none";
+	    }
+	}
+	      
 });
